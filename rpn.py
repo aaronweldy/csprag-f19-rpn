1d713ff5351a5ad03bbd35b2ac45571e4f2f4dab
aaronweldy <aweldy@umich.edu>
2019-10-30 21:19:03 -0400

    Init

---
+++ rpn.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import operator, string
+
+ops = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+
+def calculate(arg):
+    st = []
+    for sign in arg.split():
+        if sign in ops:
+            y,x = st.pop(), st.pop()
+            z = ops[sign](x,y)
+        else: 
+            z = float(sign)
+        st.append(z)
+    return(st.pop())        
+def main():
+    while True: 
+        calculate(input("rpn calc> "))
+        
+
+if __name__ == '__main__':
+    main()
